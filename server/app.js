e99daecfbe031f516a963faf49a1cf48aa95b5be
Kevin Fami Anggara <kevinfanggara@gmail.com>
2023-11-16 10:14:11 +0700

    login page implemente

--- server/app.js
+++ server/app.js
@@ -1,12 +1,14 @@
-require("dotenv").config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const express = require("express");
-const AuthController = require("./controllers/AuthController")
-const PublicController = require("./controllers/PublicController")
-const AdminController = require("./controllers/AdminController")
-const OrderController = require("./controllers/OrderController")
-const PaymentController = require("./controllers/PaymentController")
-const authentication = require("./middlewares/authentication")
-const errorHandler = require("./middlewares/errorHandler")
+const AuthController = require("./controllers/AuthController");
+const PublicController = require("./controllers/PublicController");
+const AdminController = require("./controllers/AdminController");
+const PaymentController = require("./controllers/PaymentController");
+const authentication = require("./middlewares/authentication");
+const errorHandler = require("./middlewares/errorHandler");
 const { authorizationAdmin } = require("./middlewares/authorization");
 const multer = require("multer");
 const storage = multer.memoryStorage();
@@ -22,46 +24,56 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Show all products for public
-app.get('/products', PublicController.allProducts)
+app.get("/products", PublicController.allProducts);
 
 // Show product details
-app.get('/products/:id', PublicController.productDetails)
+app.get("/products/:id", PublicController.productDetails);
 
 // User login using email & password
-app.post("/login", AuthController.login)
+app.post("/login", AuthController.login);
 
 // User register using email & password
-app.post("/register", AuthController.register)
+app.post("/register", AuthController.register);
 
 // Google login
-app.post("/google-sign-in", AuthController.googleLogin)
+app.post("/google-sign-in", AuthController.googleLogin);
 
 // Auth middleware
 app.use(authentication);
 
-// Checkout page
-// app.get('/checkout', OrderController.getCheckoutData)
-// app.put('/checkout', OrderController.submitCheckout)
-
 // Midtrans payment
-// app.get('/payment/midtrans/token', PaymentController.getMidtransToken)
+app.post("/payment/midtrans/token", PaymentController.getMidtransToken);
 
 // Midtrans notifications update status
 // app.post('/payment/midtrans/notifications', PaymentController.postMidtransNotif)
 
 // Show all products
-app.get("/admin/products", authorizationAdmin, AdminController.listProduct)
+app.get("/admin/products", authorizationAdmin, AdminController.listProduct);
 
 // Create product
-app.post("/admin/products", authorizationAdmin, upload.single('imageUrl'), AdminController.createProduct)
+app.post(
+  "/admin/products",
+  authorizationAdmin,
+  upload.single("imageUrl"),
+  AdminController.createProduct
+);
 
 // Update product
-app.put("/admin/products/:id", authorizationAdmin, upload.single('imageUrl'), AdminController.updateProduct)
+app.put(
+  "/admin/products/:id",
+  authorizationAdmin,
+  upload.single("imageUrl"),
+  AdminController.updateProduct
+);
 
 // Delete product
-app.delete("/admin/products/:id", authorizationAdmin, AdminController.deleteProduct)
+app.delete(
+  "/admin/products/:id",
+  authorizationAdmin,
+  AdminController.deleteProduct
+);
 
 // Error handler
 app.use(errorHandler);
 
-module.exports = app
+module.exports = app;
--- server/controllers/PaymentController.js
+++ server/controllers/PaymentController.js
@@ -1,3 +1,52 @@
-class PaymentController {}
+const midtransClient = require("midtrans-client");
+const randomstring = require("randomstring");
+const { Order } = require("../models/index");
+
+class PaymentController {
+  static async getMidtransToken(req, res, next) {
+    try {
+      // Create Snap API instance
+      const snap = new midtransClient.Snap({
+        // Set to true if you want Production Environment (accept real transaction).
+        isProduction: false,
+        serverKey: process.env.MIDTRANS_SERVER_KEY,
+      });
+      const orderId = `trx-${randomstring.generate()}`;
+      const orderData = await Order.create({
+        UserId: req.user.id,
+        ProductId: req.body.productId,
+        quantity: req.body.quantity,
+        amount: req.body.grossAmount,
+        status: req.body.status
+      },)
+      console.log("UserId " + req.user.id)
+      console.log("ProductId " + req.body.id);
+      const parameter = {
+        transaction_details: {
+          order_id: orderId,
+          gross_amount: req.body.grossAmount,
+        },
+        credit_card: {
+          secure: true,
+        },
+        customer_details: {
+          email: req.user.email,
+          phone: req.user.phoneNumber,
+          address: req.user.address,
+        },
+      };
+      // console.log(transaction_details);
+      snap.createTransaction(parameter).then((transaction) => {
+        // transaction token
+        let transactionToken = transaction.token;
+        // console.log("transactionToken:", transactionToken);
+        res.json({ transaction_token: transactionToken, orderId });
+      });
+    } catch (error) {
+      // console.log(error)
+      next(error);
+    }
+  }
+}
 
 module.exports = PaymentController;
